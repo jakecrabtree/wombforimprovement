2192fe391c51d83da810979bdd6288401c27e1ce
westonwright <57042424+westonwright@users.noreply.github.com>
2019-10-26 12:14:03 -0500

    Continuing rope work
    Added temporary crosshair (rope will have one attachment point in final game)

--- Assets/Scripts/RopeSystem.cs
+++ Assets/Scripts/RopeSystem.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class RopeSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject ropeHingeAnchor;
+    public DistanceJoint2D ropeJoint;
+    //Eventually need to get rid of this.
+    public Transform crosshair;
+    public SpriteRenderer crosshairSprite;
+    public PlayerController playerController;
+    //Eventually won't be used
+    private bool ropeAttached;
+    private Vector2 playerPosition;
+    private Rigidbody2D ropeHingeAnchorRb;
+    private SpriteRenderer ropeHingeAnchorSprite;
+
+    void Awake()
     {
-        
+        ropeJoint.enabled = false;
+        playerPosition = transform.position;
+        ropeHingeAnchorRb = ropeHingeAnchor.GetComponent<Rigidbody2D>();
+        ropeHingeAnchorSprite = ropeHingeAnchor.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //Used for crosshair, eventually won't need this
+        var worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+        var facingDirection = worldMousePosition - transform.position;
+        var aimAngle = Mathf.Atan2(facingDirection.y, facingDirection.x);
+        if(aimAngle < 0f)
+        {
+            aimAngle = Mathf.PI * 2 + aimAngle;
+        }
+
+        var aimDirection = Quaternion.Euler(0, 0, aimAngle * Mathf.Rad2Deg) * Vector2.right;
+        playerPosition = transform.position;
+
+        if (!ropeAttached)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
