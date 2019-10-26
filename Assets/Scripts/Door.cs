018261c865814ea5281bca87865d889e0b83d2b0
Dulworth, Matthew <mhdulworth@gmail.com>
2019-10-26 16:19:55 -0500

    added doors

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool locked = true;
+
+    public void Unlock(){
+        if(GetComponent<BoxCollider2D>() && locked){
+            locked = false;
+            Debug.Log("Door unlocked");
+            Destroy(GetComponent<BoxCollider2D>());
+        }
+    }
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    private bool activated = false;
+    [SerializeField] private Door door;
+    void OnTriggerEnter2D(Collider2D collider){
+        activated = true;
+    }
+
+    public bool Activated{
+        get{return activated;}
+    }
+
+    public Door Door{
+        get{return door;}
+    }
+}
