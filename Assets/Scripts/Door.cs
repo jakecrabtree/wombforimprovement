cd701ec9e345f9024216e80a2d61fa2d76c73f3a
Jake <jakercrabtree@utexas.edu>
2019-10-26 21:52:59 -0500

    door open/close

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     private bool locked = true;
+    private SpriteRenderer _sprite;
+
+    private void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
 
     public void Unlock(){
         if(GetComponent<BoxCollider2D>() && locked){
             locked = false;
             Debug.Log("Door unlocked");
             Destroy(GetComponent<BoxCollider2D>());
+            _sprite.sprite = Resources.Load<Sprite>("DoorOpen");
         }
     }
 }
